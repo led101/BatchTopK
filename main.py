aa9e1c82d31b740d763fe6a4bb6e144436f0cdff
Kevin Ma <kyma@usc.edu>
2025-05-01 11:31:32 -0700

    customize for evo model

--- main.py
+++ main.py
@@ -8,22 +8,27 @@ from transformer_lens import HookedTransformer
 
 for l1_coeff in [0.004, 0.0018, 0.0008]:
     cfg = get_default_cfg()
-    cfg["sae_type"] = "jumprelu" # "vanilla", "topk", "batchtopk"
-    cfg["model_name"] = "gpt2-small"
-    cfg["layer"] = 8
+    cfg["sae_type"] = "batchtopk"
+    cfg["model_name"] = "arcinstitute/evo2_7b"
+    cfg["layer"] = 26
     cfg["site"] = "resid_pre"
-    cfg["dataset_path"] = "Skylion007/openwebtext"
+    cfg["dataset_path"] = "arcinstitute/opengenome2"
     cfg["aux_penalty"] = (1/32)
     cfg["lr"] = 3e-4
     cfg["input_unit_norm"] = True
     cfg["top_k"] = 32
-    cfg["dict_size"] = 768 * 16
-    cfg['wandb_project'] = 'batchtopk_comparison'
-    cfg['act_size'] = 768
+    cfg['wandb_project'] = 'evo2_sae_analysis'
     cfg['device'] = 'cuda'
     cfg['bandwidth'] = 0.001
     cfg['l1_coeff'] = l1_coeff
+    cfg["act_size"] = 4096  # hidden size of 7b model
+    cfg["dict_size"] = 4096 * 16
 
+    cfg = post_init_cfg(cfg)
+                
+    model = HookedTransformer.from_pretrained(cfg["model_name"]).to(cfg["dtype"]).to(cfg["device"])
+    activations_store = ActivationsStore(model, cfg)
+    
     if cfg["sae_type"] == "vanilla":
         sae = VanillaSAE(cfg)
     elif cfg["sae_type"] == "topk":
@@ -32,81 +37,4 @@ for l1_coeff in [0.004, 0.0018, 0.0008]:
         sae = BatchTopKSAE(cfg)
     elif cfg["sae_type"] == 'jumprelu':
         sae = JumpReLUSAE(cfg)
-
-    cfg = post_init_cfg(cfg)
-                
-    model = HookedTransformer.from_pretrained(cfg["model_name"]).to(cfg["dtype"]).to(cfg["device"])
-    activations_store = ActivationsStore(model, cfg)
     train_sae(sae, activations_store, model, cfg)
-
-for sae_type in ['topk', 'batchtopk']:
-    for top_k in [16, 32, 64]:
-        cfg = get_default_cfg()
-        cfg["sae_type"] = sae_type
-        cfg["model_name"] = "gpt2-small"
-        cfg["layer"] = 8
-        cfg["site"] = "resid_pre"
-        cfg["dataset_path"] = "Skylion007/openwebtext"
-        cfg["aux_penalty"] = (1/32)
-        cfg["lr"] = 3e-4
-        cfg["input_unit_norm"] = True
-        cfg["top_k"] = 32
-        cfg["dict_size"] = 768 * 16
-        cfg['wandb_project'] = 'batchtopk_comparison'
-        cfg['l1_coeff'] = 0.
-        cfg['act_size'] = 768
-        cfg['device'] = 'cuda'
-        cfg['bandwidth'] = 0.001
-        cfg['top_k'] = top_k
-
-        if cfg["sae_type"] == "vanilla":
-            sae = VanillaSAE(cfg)
-        elif cfg["sae_type"] == "topk":
-            sae = TopKSAE(cfg)
-        elif cfg["sae_type"] == "batchtopk":
-            sae = BatchTopKSAE(cfg)
-        elif cfg["sae_type"] == 'jumprelu':
-            sae = JumpReLUSAE(cfg)
-
-        cfg = post_init_cfg(cfg)
-                    
-        model = HookedTransformer.from_pretrained(cfg["model_name"]).to(cfg["dtype"]).to(cfg["device"])
-        activations_store = ActivationsStore(model, cfg)
-        train_sae(sae, activations_store, model, cfg)
-
-
-for sae_type in ['topk', 'batchtopk']:
-    # don't retrain *16
-    for dict_size in [768*4, 768*8, 768*32]:
-        cfg = get_default_cfg()
-        cfg["sae_type"] = sae_type
-        cfg["model_name"] = "gpt2-small"
-        cfg["layer"] = 8
-        cfg["site"] = "resid_pre"
-        cfg["dataset_path"] = "Skylion007/openwebtext"
-        cfg["aux_penalty"] = (1/32)
-        cfg["lr"] = 3e-4
-        cfg["input_unit_norm"] = True
-        cfg["top_k"] = 32
-        cfg["dict_size"] = dict_size
-        cfg['wandb_project'] = 'batchtopk_comparison'
-        cfg['l1_coeff'] = 0.
-        cfg['act_size'] = 768
-        cfg['device'] = 'cuda'
-        cfg['bandwidth'] = 0.001
-        cfg['top_k'] = 32
-
-        if cfg["sae_type"] == "vanilla":
-            sae = VanillaSAE(cfg)
-        elif cfg["sae_type"] == "topk":
-            sae = TopKSAE(cfg)
-        elif cfg["sae_type"] == "batchtopk":
-            sae = BatchTopKSAE(cfg)
-        elif cfg["sae_type"] == 'jumprelu':
-            sae = JumpReLUSAE(cfg)
-
-        cfg = post_init_cfg(cfg)
-                    
-        model = HookedTransformer.from_pretrained(cfg["model_name"]).to(cfg["dtype"]).to(cfg["device"])
-        activations_store = ActivationsStore(model, cfg)
-        train_sae(sae, activations_store, model, cfg)
