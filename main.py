ddda7dd29d929f29ea00aa734fb7193bb717ca6d
Kevin Ma <kyma@usc.edu>
2025-05-07 00:00:44 -0700

    Oops. Not supposed to have commas

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ for l1_coeff in [0.004, 0.0018, 0.0008]:
     cfg["model_name"] = "evo2_7b"
     cfg["layer"] = 26
     cfg["site"] = "resid_pre"
-    evo2_hook_module_path = "blocks.25.post_norm",
+    evo2_hook_module_path = "blocks.25.post_norm"
     cfg["dataset_path"] = "arcinstitute/opengenome2"
     cfg["act_size"] = 4096  # hidden size of 7b model
     cfg["dict_size"] = 4096 * 16
     cfg["top_k"] = 32
     cfg['l1_coeff'] = l1_coeff
-    cfg['seq_len'] = 512,
-    cfg['model_batch_size'] = 256,
-    cfg['dtype'] = torch.bfloat16,
-    cfg['device'] = 'cuda',
+    cfg['seq_len'] = 512
+    cfg['model_batch_size'] = 256
+    cfg['dtype'] = torch.bfloat16
+    cfg['device'] = 'cuda'
 
     cfg["aux_penalty"] = (1/32)
     cfg["lr"] = 3e-4
@@ -32,5 +32,5 @@ for l1_coeff in [0.004, 0.0018, 0.0008]:
     cfg = post_init_cfg(cfg)
 
     sae  = BatchTopKSAE(cfg).to(cfg["device"])
-    acts = Evo2ActivationsStore(cfg)             # <‑‑ new
-    train_sae(sae, acts, None, cfg)              # model‑arg can be None
+    acts = Evo2ActivationsStore(cfg)             
+    train_sae(sae, acts, None, cfg)              
